bc36aa5f6d5fbd5d633d368276afcd1aa970a727
Tilon <76069479+Kingtilon1@users.noreply.github.com>
2021-08-05 17:07:42 -0400

    Create dbconfig.js

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config={
+    user :'mirac',
+    password :'Kidace909',
+    server:'127.0.0.1',
+    database:'[dbo].[tbl_Employee]',
+    options:{
+        trustedconnection: true,
+        enableArithPort : true,
+        instanceName :'DESKTOP-RN0V0U0'
+    },
+    port : 1433
+}
+
+module.exports = config;
