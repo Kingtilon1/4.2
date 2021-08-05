ea3bf3a8f3f2a1c49f4f282cc4695bd1f8a1b2b0
Tilon <76069479+Kingtilon1@users.noreply.github.com>
2021-08-05 17:08:21 -0400

    Create order.js

---
+++ order.js
@@ -0,0 +1,10 @@
+class Order{
+    constructor(FirstName,MiddleName,LastName,Age){
+        this.FirstName=FirstName;
+        this.MiddleName=MiddleName;
+        this.LastName=LastName;
+        this.Age=Age;
+    }
+}
+
+module.exports = Order;
